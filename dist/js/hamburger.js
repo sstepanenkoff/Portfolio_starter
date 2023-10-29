a49b272eca3a9384798247220c03d55af7bf004c
Sergey Stepanenko <71725778+sstepanenkoff@users.noreply.github.com>
2023-10-29 19:35:49 +0100

    start develop second block

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -1,11 +1,7 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
-
+const hamburger = document.querySelector('.hamburger'), menu = document.querySelector('.menu'), closeElem = document.querySelector('.menu__close');
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
-
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
